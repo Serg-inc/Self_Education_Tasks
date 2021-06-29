b177957aa9277b18633f621c21c889a248e00b55
Serhii Neuimn <sergneuimin@gmail.com>
2021-06-29 09:06:13 +0300

    add func Fibonacci num

--- Task2_RecursiveFib/recfib_main.cpp
+++ Task2_RecursiveFib/recfib_main.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+int FibonacciNum(int index)
+{
+	if (index == 0) return 0;
+	else if (index == 1) return 1;
+	else return FibonacciNum(index - 1) + FibonacciNum(index - 2);
+};
+
+
 int  main()
 {
+    int indx = 0;    
+    cout << "Enter index of Fibonacci Num:" << "\n";
+    cin >> indx;
+    cout << "Fibonacci number: " << FibonacciNum(indx) << "\n";
+
+    cout << "Calc sum of first n element of Fibonacci number" << "\n";
+    cout << "Enter n: ";
+
 
     cin.get(); // click enter for exit
     return 0;
 }
+
+
