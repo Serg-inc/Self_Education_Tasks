f85b3017cf5859faf364d23fc9b68a8a024b05a7
Serhii Neuimn <sergneuimin@gmail.com>
2021-06-29 10:07:42 +0300

    files in folder

--- Task3_PosixThreads/pthread_main.cpp
+++ Task3_PosixThreads/pthread_main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
 
 using namespace std;
 
 int  main()
 {
+    WIN32_FIND_DATAA data;
+    std::string dir = "c:\\* ";
+    HANDLE hFind = FindFirstFileA(dir.c_str(), &data);      // DIRECTORY
 
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            std::cout << data.cFileName << std::endl;
+        } while (FindNextFileA(hFind, &data));
+        FindClose(hFind);
+    }
 }
 
 
