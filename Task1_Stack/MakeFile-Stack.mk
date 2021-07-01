d145d2bc950cb75b89c011fc87f6c58da3a256dd
Serhii Neuimn <sergneuimin@gmail.com>
2021-07-01 14:11:12 +0300

    update makefile, add macro

--- Task1_Stack/MakeFile-Stack.mk
+++ Task1_Stack/MakeFile-Stack.mk
@@ -1,11 +1,17 @@
 CC=g++ 
 CFLAGS= -Wall -g #Flag for implicit rules. Turn on debug info
 
-stack: stack_main.o 
-	$(CC) $(CFLAGS)  -o  stack stack_main.o 
+SOURCES=stack_main.cpp 
+OBJECTS=$(SOURCES:.cpp=.o)
+EXECUTABLE=stack
 
-stack_main.o: stack_main.cpp stack_template.h
-	$(CC) $(CFLAGS)  -c stack_main.cpp 
 
-clean :
-	rm  stack_main.o  stack.exe
+$(EXECUTABLE): $(OBJECTS)
+	$(CC) $(CFLAGS)	-o $(EXECUTABLE) $(OBJECTS)
+
+$(OBJECTS): $(SOURCES) 
+	$(CC) $(CFLAGS) -c $(SOURCES)
+
+clean:
+	rm  *.o *.exe
+
